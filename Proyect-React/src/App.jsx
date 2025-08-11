91caf88517f59dbc448f2cb6dbaf668343c3ed67
CristoperAcosta <crisacosta2005@gmail.com>
2025-08-10 21:22:10 -0300

    Create-FormUser

--- Proyect-React/src/App.jsx
+++ Proyect-React/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router'
+import LoginForm from './Components/LoginForm/LoginForm'
 
 function App() {
 
   return (
     <div>
       <Routes>
-        <Route path='/' element = { <h1>Hola mundo</h1> }/>
+        <Route path='/' element = { <LoginForm/> }/>
       </Routes>
     </div>
   )
